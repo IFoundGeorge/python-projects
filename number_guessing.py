6bface8a83539d01246b8ea4093fb07633e94888
Cyrus Cavero <caverocyrus@gmail.com>
2026-02-26 14:41:43 +1100

    Number Guessing

---
+++ number_guessing.py
@@ -0,0 +1,26 @@
+import os
+import random
+
+while True:
+    os.system('cls')
+
+    num = random.randint(1, 10)
+    print(num)
+
+    print("Welcome to the Number Guessing Game!"
+            " I'm thinking of a number between 1 and 10. \n")
+
+    x = input("What number Am I thinking of? : ")
+
+    if num == int(x):
+        print("Congratulations! You guessed the number correctly! \n")
+    else:
+        print(f"Sorry, the correct number was {num}. Better luck next time! \n")
+
+    # your code
+    cont = input("Another one? yes/no > ")
+
+    if cont == "no":
+        print("Break")
+        break
+
